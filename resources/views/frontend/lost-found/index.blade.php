fe443d84692e452aee3b6c9a98b0ee28ed4b28db
Daffa <fdaffa520@gmail.com>
2025-12-28 22:03:19 +0700

    Fix eror image not found

--- resources/views/frontend/lost-found/index.blade.php
+++ resources/views/frontend/lost-found/index.blade.php
@@ -27,7 +27,7 @@
                     
                     <div class="relative h-48 w-full bg-gray-200">
                         @if($item->image)
-                            <img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->title }}" class="w-full h-full object-cover">
+                            <img src="{{ asset( 'storage/' .  $item->image) }}" alt="{{ $item->title }}" class="w-full h-full object-cover">
                         @else
                             <div class="flex items-center justify-center h-full text-gray-400 flex-col">
                                 <svg class="w-12 h-12 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
